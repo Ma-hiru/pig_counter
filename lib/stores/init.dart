764073fcbb154eaa1ce3d3bd2c5a60f9881e332c
Ma-hiru <tenshimahiru@qq.com>
2026-03-07 04:05:59 +0800

    更新 init.dart

--- lib/stores/init.dart
+++ lib/stores/init.dart
@@ -2,7 +2,7 @@ import 'package:get/get.dart';
 import 'package:pig_counter/stores/settings.dart';
 import 'package:pig_counter/stores/user.dart';
 
-Future initGetXStore() async {
+void initGetXStore() {
   Get.put(UserController(), permanent: true);
   Get.put(SettingsController(), permanent: true);
 }
